0b772246c1d7089722e803fdc3f32c4010c09d1f
mlkelly <maurak87@gmail.com>
2020-06-23 16:56:25 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
+
+  before_action :current_artist, only: [:show, :edit, :update, :destory]
+
   def index
+    @artists= Artist.all
   end
 
-  def show
+  def show 
   end
 
   def new
@@ -20,11 +24,9 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
 
     @artist.update(artist_params)
 
@@ -36,7 +38,6 @@ class ArtistsController < ApplicationController
   end
 
   def destroy
-    @artist = Artist.find(params[:id])
     @artist.destroy
     flash[:notice] = "Artist deleted."
     redirect_to artists_path
@@ -46,5 +47,9 @@ class ArtistsController < ApplicationController
 
   def artist_params
     params.require(:artist).permit(:name)
+  end
+
+  def current_artist
+    @artist = Artist.find(params[:id])
   end
 end
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,8 @@
 class SongsController < ApplicationController
+  before_action :current_song, only: [:show, :edit, :update, :destory]
+
   def index
+    @songs = Song.all
   end
 
   def show
@@ -21,11 +24,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
 
     @song.update(song_params)
 
@@ -37,7 +38,6 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     flash[:notice] = "Song deleted."
     redirect_to songs_path
@@ -52,4 +52,9 @@ class SongsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def current_song
+    @song = Song.find(params[:id])
+  end
+
 end
